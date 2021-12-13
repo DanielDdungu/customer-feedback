d0f619041bb550c7d96711e5598becdb82feeaef
DanielDdungu <daddungu@outlook.com>
2021-12-14 00:33:05 +0100

    Data exchange

---
+++ src/Components/FeedbackForm.jsx
@@ -0,0 +1,63 @@
+import Button from "./shared/Button";
+import Card from "./shared/Card";
+import RatingSelect from "./RatingSelect";
+import { useState } from "react";
+
+function FeedbackForm({ handleAdd }) {
+  const [text, setText] = useState("");
+  const [rating, setRating] = useState("");
+  const [btnDisabled, setBtnDisabled] = useState(true);
+  const [message, setMessage] = useState("");
+
+  const handleTextChange = (e) => {
+    if (text === "") {
+      setBtnDisabled(true);
+      setMessage(null);
+    } else if (text !== "" && text.trim().length <= 10) {
+      setMessage("Text must be greater than or equal to 10 Characters");
+      setBtnDisabled(true);
+    } else {
+      setMessage(null);
+      setBtnDisabled(false);
+    }
+    setText(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (text.trim().length > 10) {
+      const newFeedback = {
+        text,
+        rating,
+      };
+      handleAdd(newFeedback);
+
+      setText("");
+    }
+  };
+
+  return (
+    <Card>
+      <form onSubmit={handleSubmit}>
+        <h2>How do you rate our service with us Daniel</h2>
+        {/* @todo rating component */}
+        <RatingSelect select={(rating) => setRating(rating)} />
+        <div className="input-group">
+          <input
+            onChange={handleTextChange}
+            type="text"
+            placeholder="Write a review"
+            value={text}
+          />
+          <Button type="submit" isDisabled={btnDisabled}>
+            Send
+          </Button>
+        </div>
+
+        {message && <div className="message"></div>}
+      </form>
+    </Card>
+  );
+}
+
+export default FeedbackForm;
