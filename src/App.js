52ab2f6505e4fb7d05bb585176a77b0ff76b13e3
DanielDdungu <daddungu@outlook.com>
2021-12-13 13:10:45 +0100

    FeedbackStats

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
-import FeedbackData from './data/FeedbackData'
-import FeedbackList from './components/FeedbackList'
-import Header from './components/Header';
-import React from 'react';
-import { useState } from 'react'
+import FeedbackData from "./data/FeedbackData";
+import FeedbackList from "./components/FeedbackList";
+import Header from "./components/Header";
+import React from "react";
+import { useState } from "react";
 
 function App() {
-    const [feedback, setFeedback] = useState()
+  const [feedback, setFeedback] = useState(FeedbackData);
 
-    return(
-        <>
-        <Header />
-        <div className="container">
-            <FeedbackList  feedback={feedback} />
-        </div>
-        </>
-    )
+  const deleteFeedback = (id) => {
+    if (window.confirm("Are you sure you want to delete")) {
+      setFeedback(feedback.filter((item) => item.id !== id));
+    }
+  };
+
+  return (
+    <>
+      <Header />
+      <div className="container">
+        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+      </div>
+    </>
+  );
 }
-
-
 
 export default App;
--- src/Components/FeedbackItem.jsx
+++ src/Components/FeedbackItem.jsx
@@ -1,12 +1,20 @@
+import Card from "./shared/Card";
+import { FaTimes } from "react-icons/fa";
+import PropTypes from "prop-types";
 import React from "react";
 
-function FeedbackItem({ item }) {
+function FeedbackItem({ item, handleDelete }) {
   return (
-    <div className="card">
+    <Card>
       <div className="num-display">{item.rating}</div>
+      <button onClick={() => handleDelete(item.id)} className="close">
+        <FaTimes color="purple" />
+      </button>
       <div className="text-display">{item.text}</div>
-    </div>
+    </Card>
   );
 }
+
+FeedbackItem.propTypes = { item: PropTypes.object.isRequired };
 
 export default FeedbackItem;
