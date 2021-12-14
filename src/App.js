d15c4b86da2f3a9c04ab5c284ee6024e1d9dddf6
DanielDdungu <daddungu@outlook.com>
2021-12-14 18:28:57 +0100

    Final

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+
+import AboutPage from "./pages/AboutPage";
 import FeedbackData from "./data/FeedbackData";
+import FeedbackForm from "./components/FeedbackForm";
 import FeedbackList from "./components/FeedbackList";
+import FeedbackStats from "./components/FeedbackStats";
 import Header from "./components/Header";
 import React from "react";
 import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 function App() {
+  const addFeedback = (newFeedback) => {
+    newFeedback.id = uuidv4();
+    setFeedback([newFeedback, ...feedback]);
+  };
   const [feedback, setFeedback] = useState(FeedbackData);
 
   const deleteFeedback = (id) => {
@@ -14,12 +24,28 @@ function App() {
   };
 
   return (
-    <>
+    <Router>
       <Header />
       <div className="container">
-        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+        <Routes>
+          <Route
+            exact
+            path="/"
+            element={
+              <>
+                <FeedbackForm handleAdd={addFeedback} />
+                <FeedbackStats feedback={feedback} />
+                <FeedbackList
+                  feedback={feedback}
+                  handleDelete={deleteFeedback}
+                />
+              </>
+            }
+          ></Route>
+          <Route path="/about" element={<AboutPage />} />
+        </Routes>
       </div>
-    </>
+    </Router>
   );
 }
 
---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,19 @@
+import Card from "../components/shared/Card";
+
+function AboutPage() {
+  return (
+    <Card>
+      <div className="about">
+        <h1>About this project</h1>
+        <p>This is a React App to leave customer feedback for a product</p>
+        <p>Version: 1.0.0</p>
+
+        <p>
+          <h1>hello</h1>
+        </p>
+      </div>
+    </Card>
+  );
+}
+
+export default AboutPage;
