771cb994b9c94aa2b31a6efb6f73cfa255c7a93a
DanielDdungu <daddungu@outlook.com>
2021-12-21 17:34:53 +0100

    changes

--- src/Components/AboutIconLink.jsx
+++ src/Components/AboutIconLink.jsx
@@ -2,7 +2,7 @@ import { IoArrowUpCircleOutline } from "react-icons/io5";
 import { Link } from "react-router-dom";
 function AboutIconLink() {
   return (
-    <div ClassName="about-link">
+    <div className="about-link">
       <Link to="/about">
         <IoArrowUpCircleOutline size={30} />
       </Link>
